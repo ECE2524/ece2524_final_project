2a3ad991062d720b709abe42c7726122f7723368
jpenn92 <jpenn92@vt.edu>
2013-12-02 22:45:18 -0500

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,25 @@
+#include "player.h"
+
+player::player()
+{
+
+}
+
+player::~player()
+{
+
+}
+
+void player::addInventory(string newItem)
+{
+  inventory.push_back(newItem);
+}
+
+void player::printInventory()
+{
+  cout << "Your inventory:" << endl;
+  for(list<int>::const_iterator iterator = inventory.begin(), end = inventory.end(); iterator != end; ++iterator)
+  {
+    cout << *iterator << endl;
+  }
+}
