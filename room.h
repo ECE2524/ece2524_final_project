96a53a9877b7bb69379cc33abb62bf767b40abb7
jpenn92 <jpenn92@vt.edu>
2013-12-02 22:36:34 -0500

    Update room.h

--- room.h
+++ room.h
@@ -11,26 +11,3 @@ public:
   void addItem(string);
   void printItems();
 }
-
-room::room()
-{
-
-}
-
-room::~room()
-{
-
-}
-
-void room::addItem(string newItem)
-{
-  items.push_back(newItem);
-}
-
-void room::printItems()
-{
-  for(list<int>::const_iterator iterator = intList.begin(), end = intList.end(); iterator != end; ++iterator)
-  {
-    cout << *iterator;
-  }
-}
