d294d08b060676979b81613506aa6c7657da9828
jpenn92 <jpenn92@vt.edu>
2013-12-06 16:05:54 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -10,6 +10,18 @@ game::~game()
 
 }
 
+voide game::gameInit()
+{
+  room bedroom;
+  bedroom.setName("bedroom");
+  bedroom.setDescription("You are in a dreary bedroom with white walls and ceiling.  You look around to find a few items.");
+  bedroom.addAdjacentRoom("hallway");
+  bedroom.addItem("syringe");
+  bedroom.addItem("shirt");
+  bedroom.addItem("pants");
+  bedroom.addItem("shoes");
+}
+
 void game::getInput()
 {
   input.clear();
@@ -31,4 +43,18 @@ void game::getInput()
     object += input[i];
     i++
   }
+}
+
+bool game::checkInput()
+{
+  if(command == "move")
+  {
+    return TRUE;
+  }
+  else if(command == "pickup")
+  {
+    return TRUE;
+  }
+  else
+    return FALSE;
 }
