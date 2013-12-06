d3858c67a9ff60c16b54d0d165b71b6f8390daa9
jpenn92 <jpenn92@vt.edu>
2013-12-06 09:55:22 -0500

    Update room.h

--- room.h
+++ room.h
@@ -4,10 +4,16 @@
 class room
 {
 private:
+  string name;
+  string description;
   list<string> items;
 public:
   room();
   ~room();
   void addItem(string);
   void printItems();
+  void room setName(string);
+  void room printName();
+  void room setDescription(string);
+  void room printDescription();
 };
