c59fc439a35cd286b1a62d281497571d8f70e269
jpenn92 <jpenn92@vt.edu>
2013-12-10 23:08:21 -0500

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -19,11 +19,12 @@ void room::addItem(string newItem)
 
 void room::printItems()
 {
-  cout << "Room items:" << endl;
+  cout << "Room items:\n";
   for(list<string>::const_iterator iterator = items.begin(), end = items.end(); iterator != end; ++iterator)
   {
-    cout << *iterator << endl;
+    cout << *iterator << "\n";
   }
+  cout << "\n";
 }
 
 void room::addAdjacentRoom(string newRoom)
@@ -31,13 +32,24 @@ void room::addAdjacentRoom(string newRoom)
   adjacentRooms.push_back(newRoom);
 }
 
+bool room::isAdjacentRoom(string nextRoom)
+{
+	for(list<string>::const_iterator iterator = adjacentRooms.begin(), end = adjacentRooms.end(); iterator != end; ++iterator)
+	{
+		if(*iterator == nextRoom)
+			return 1;
+	}
+	return 0;
+}
+
 void room::printAdjacentRooms()
 {
-  cout << "Adjacent rooms:" << endl;
+  cout << "Adjacent rooms:" << "\n";
   for(list<string>::const_iterator iterator = adjacentRooms.begin(), end = adjacentRooms.end(); iterator != end; ++iterator)
   {
-    cout << *iterator << endl;
+    cout << *iterator << "\n";
   }
+  cout << "\n";
 }
 
 void room::setName(string newName)
@@ -47,7 +59,7 @@ void room::setName(string newName)
 
 void room::printName()
 {
-  cout << name << endl;
+  cout << "Room name: " << name << "\n\n";
 }
 
 void room::setDescription(string newDescription)
@@ -55,7 +67,7 @@ void room::setDescription(string newDescription)
   description = newDescription;
 }
 
-void room::pringDescription()
+void room::printDescription()
 {
-  cout << description << endl;
+  cout << description << "\n\n";
 }
