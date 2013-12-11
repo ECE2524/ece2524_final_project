375461429a723607b5dd962c3a1dfe4264cd75ed
jpenn92 <jpenn92@vt.edu>
2013-12-11 00:33:51 -0500

    Added comments to room.h

--- room.h
+++ room.h
@@ -6,20 +6,35 @@ using namespace std;
 class room
 {
 private:
+  // linked list contains room items
   list<string> items;
+  // linked list contains adjacent rooms
   list<string> adjacentRooms;
+  // holds the name of the room
   string name;
+  // holds the room description
   string description;
 public:
+  // class constructor
   room();
+  // class destructor
   ~room();
+  // add an item to the room
   void addItem(string);
+  // print the items in a room
   void printItems();
+  // add an ajacent room
   void addAdjacentRoom(string);
+  // check if a room is adjacent to a room
   bool isAdjacentRoom(string);
+  // print all adjacent rooms
   void printAdjacentRooms();
+  // set the name of a room
   void setName(string);
+  // print the name of a room
   void printName();
+  // set the description of a room
   void setDescription(string);
+  // print the description of a room
   void printDescription();
 };
