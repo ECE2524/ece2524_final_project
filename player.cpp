b6c1cb4f74550c39153fdbd558e3c00c94464842
jpenn92 <jpenn92@vt.edu>
2013-12-11 00:29:56 -0500

    Added comments to player.cpp

--- player.cpp
+++ player.cpp
@@ -1,20 +1,24 @@
 #include "player.h"
 
+// class constructor
 player::player()
 {
 
 }
 
+// class destructor
 player::~player()
 {
 
 }
 
+// add an item to the player inventory
 void player::addInventory(string newItem)
 {
   inventory.push_back(newItem);
 }
 
+// check if an item is in the player inventory
 bool player::isInventory(string useItem)
 {
 	for(list<string>::const_iterator iterator = inventory.begin(), end = inventory.end(); iterator != end; ++iterator)
@@ -25,6 +29,7 @@ bool player::isInventory(string useItem)
 	return 0;
 }
 
+// print the items the player inventory
 void player::printInventory()
 {
   cout << "Your inventory:" << endl;
