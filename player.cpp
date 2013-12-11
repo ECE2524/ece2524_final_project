7b3d4ad7ea0cbfb1fed734f4e43d91f6f20ac7eb
jpenn92 <jpenn92@vt.edu>
2013-12-10 23:05:22 -0500

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -15,6 +15,16 @@ void player::addInventory(string newItem)
   inventory.push_back(newItem);
 }
 
+bool player::isInventory(string useItem)
+{
+	for(list<string>::const_iterator iterator = inventory.begin(), end = inventory.end(); iterator != end; ++iterator)
+	{
+		if(*iterator == useItem)
+			return 1;
+	}
+	return 0;
+}
+
 void player::printInventory()
 {
   cout << "Your inventory:" << endl;
