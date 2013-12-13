761bfe19407342e37f9d377c4a97a40749391629
Jay Pennington <jpenn92@vt.edu>
2013-12-13 16:28:15 -0500

    changed tabbed indenting

--- room.cpp
+++ room.cpp
@@ -5,36 +5,36 @@
 // class constructor
 room::room()
 {
-
+	
 }
 
 // class destructor
 room::~room()
 {
-
+	
 }
 
 // add an item to the room
 void room::addItem(string newItem)
 {
-  items.push_back(newItem);
+	items.push_back(newItem);
 }
 
 // print the items in a room
 void room::printItems()
 {
-  cout << "Room items:\n";
-  for(list<string>::const_iterator iterator = items.begin(), end = items.end(); iterator != end; ++iterator)
-  {
-    cout << *iterator << "\n";
-  }
-  cout << "\n";
+	cout << "Room items:\n";
+	for(list<string>::const_iterator iterator = items.begin(), end = items.end(); iterator != end; ++iterator)
+	{
+		cout << *iterator << "\n";
+	}
+	cout << "\n";
 }
 
 // add an ajacent room
 void room::addAdjacentRoom(string newRoom)
 {
-  adjacentRooms.push_back(newRoom);
+	adjacentRooms.push_back(newRoom);
 }
 
 // check if a room is adjacent to a room
@@ -51,34 +51,34 @@ bool room::isAdjacentRoom(string nextRoom)
 // print all adjacent rooms
 void room::printAdjacentRooms()
 {
-  cout << "Adjacent rooms:" << "\n";
-  for(list<string>::const_iterator iterator = adjacentRooms.begin(), end = adjacentRooms.end(); iterator != end; ++iterator)
-  {
-    cout << *iterator << "\n";
-  }
-  cout << "\n";
+	cout << "Adjacent rooms:" << "\n";
+	for(list<string>::const_iterator iterator = adjacentRooms.begin(), end = adjacentRooms.end(); iterator != end; ++iterator)
+	{
+		cout << *iterator << "\n";
+	}
+	cout << "\n";
 }
 
 // set the name of a room
 void room::setName(string newName)
 {
-  name = newName;
+	name = newName;
 }
 
 // print the name of a room
 void room::printName()
 {
-  cout << "Room name: " << name << "\n\n";
+	cout << "Room name: " << name << "\n\n";
 }
 
 // set the description of a room
 void room::setDescription(string newDescription)
 {
-  description = newDescription;
+	description = newDescription;
 }
 
 // print the description of a room
 void room::printDescription()
 {
-  cout << description << "\n\n";
+	cout << description << "\n\n";
 }
