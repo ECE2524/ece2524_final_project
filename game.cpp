f14e86b8101f26558c471e49192dd1ad91b1e823
MattP107 <mp1138@vt.edu>
2013-12-13 16:29:31 -0500

    Update game.cpp
    changed formatting for easier reading

--- game.cpp
+++ game.cpp
@@ -15,103 +15,103 @@ game::~game()
 // initializes the game
 void game::gameInit()
 {
-   /* example room
-  room bedroom;
-  bedroom.setName("bedroom");
-  bedroom.setDescription("You are in a dreary bedroom with white walls and ceiling.  You look around to find a few items.");
-  bedroom.addAdjacentRoom("hallway");
-  bedroom.addItem("syringe");
-  bedroom.addItem("shirt");
-  bedroom.addItem("pants");
-  bedroom.addItem("shoes");
-  */
+	/* example room
+	room bedroom;
+	bedroom.setName("bedroom");
+	bedroom.setDescription("You are in a dreary bedroom with white walls and ceiling.  You look around to find a few items.");
+	bedroom.addAdjacentRoom("hallway");
+	bedroom.addItem("syringe");
+	bedroom.addItem("shirt");
+	bedroom.addItem("pants");
+	bedroom.addItem("shoes");
+	*/
 
-  patientroom1.setName("patient room 1");
-  patientroom1.setDescription("Entering the room you feel a slight breeze as the window is cracked. The tattered curtains sway back-and-forth projecting shadows on the opposing wall. On the night-stand you find a drawing of a horribly disfigured man.");
-  patientroom1.addAdjacentRoom("hallway 2nd floor");
-  patientroom1.addItem("drawing");
-    
-  patientroom2.setName("patient room 2");
-  patientroom2.setDescription("This room feels familiar to you. On the ground there is a journal with all of the pages but one removed. The page states, ‘They know about us’.");
-  patientroom2.addAdjacentRoom("hallway 2nd floor");
-  patientroom2.addItem("journal");
+	patientroom1.setName("patient room 1");
+	patientroom1.setDescription("Entering the room you feel a slight breeze as the window is cracked. The tattered curtains sway back-and-forth projecting shadows on the opposing wall. On the night-stand you find a drawing of a horribly disfigured man.");
+	patientroom1.addAdjacentRoom("hallway 2nd floor");
+	patientroom1.addItem("drawing");
+	    
+	patientroom2.setName("patient room 2");
+	patientroom2.setDescription("This room feels familiar to you. On the ground there is a journal with all of the pages but one removed. The page states, ‘They know about us’.");
+	patientroom2.addAdjacentRoom("hallway 2nd floor");
+	patientroom2.addItem("journal");
+	
+	patientroom3.setName("patient room 3");
+	patientroom3.setDescription("The room is empty and there are hundreds of scratches and marks on the wooden-paneled walls. Among these marks you notice 4 distinct combinations of letters. ‘SN’, ‘AK’,  ‘EB’, and  IT’.");
+	patientroom3.addAdjacentRoom("hallway 2nd floor");
+	patientroom3.addItem("letter combination");
+	
+	bathroom.setName("bathroom");
+	bathroom.setDescription("A mirror is located on the adjacent wall to the entrance. Looking into it you see yourself. With blood-shot eyes and veins protruding from your neck, you stare in disbelief. You see a shadow walk behind you in the mirror, but turn around to see nothing.");
+	bathroom.addAdjacentRoom("hallway 2nd floor");
+	bathroom.addItem("");
+	
+	hallway2ndfloor.setName("hallway 2nd floor");
+	hallway2ndfloor.setDescription("");
+	hallway2ndfloor.addAdjacentRoom("patient room 1");
+	hallway2ndfloor.addAdjacentRoom("patient room 2");
+	hallway2ndfloor.addAdjacentRoom("patient room 3");
+	hallway2ndfloor.addAdjacentRoom("bathroom");
+	hallway2ndfloor.addAdjacentRoom("stairs");
+	hallway2ndfloor.addItem("");
+	
+	stairs.setName("stairs");
+	stairs.setDescription("");
+	stairs.addAdjacentRoom("hallway 2nd floor");
+	stairs.addAdjacentRoom("lobby");
+	stairs.addItem("");
+	
+	lobby.setName("lobby");
+	lobby.setDescription("What once was a nice entrance to the building is in disarray. Planted pots have been turned and the windows are shattered. Blood stains lead out of the building and a severed head is in the corner of the room.");
+	lobby.addAdjacentRoom("hallway 1st floor");
+	lobby.addAdjacentRoom("courtyard");
+	lobby.addAdjacentRoom("stairs");
+	lobby.addItem("severed head");
+	
+	wardenoffice.setName("warden office");
+	wardenoffice.setDescription("The room is nicely decorated with rich colored wood and a large leather chair. From the ceiling, the entrails of an unknown creature hang. Below this there is a pile of blood-splattered documents. One detailing the layout of the building.");
+	wardenoffice.addAdjacentRoom("hallway 1st floor");
+	wardenoffice.addItem("map");
+	
+	lab.setName("lab");
+	lab.setDescription("On the wall is a diagram of the human body with marks that seem to pin-point locations to inject some kind of medication. The opposing side of the room has a table with empty vials and a patient listing. The listing details information about the different patients and what room they belong to on the second floor.");
+	lab.addAdjacentRoom("hallway 1st floor");
+	lab.addItem("patient listing");
+	
+	operatingroom.setName("operating room");
+	operatingroom.setDescription("An examination table is centered in the room with demolished leather restraints. On the ground are numerous syringes that are empty.");
+	operatingroom.addAdjacentRoom("hallway 1st floor");
+	operatingroom.addItem("empty syringes");
+	
+	// starting room
+	solitaryconfinement.setName("solitary confinement");
+	solitaryconfinement.setDescription("You have awoken to find yourself laying in the middle of a white-padded room. The single light-bulb above you flickers inconsistently, and there are no signs of life around you. Across from you there is a battered door that appears to be locked.");
+	solitaryconfinement.addAdjacentRoom("hallway 1st floor");
+	solitaryconfinement.addItem("hair pin");
+	solitaryconfinement.addItem("tattered clothing");
+	
+	hallway1stfloor.setName("hallway 1st floor");
+	hallway1stfloor.setDescription("");
+	hallway1stfloor.addAdjacentRoom("lab");
+	hallway1stfloor.addAdjacentRoom("operating room");
+	hallway1stfloor.addAdjacentRoom("solitary confinement");
+	hallway1stfloor.addAdjacentRoom("warden office");
+	hallway1stfloor.addAdjacentRoom("lobby");
+	hallway1stfloor.addItem("");
+	
+	courtyard.setName("courtyard");
+	courtyard.setDescription("It is slightly raining and there are muddy footprints leading out of the now closed front gate. The entire yard is surrounded by a large fence topped with barbed wire. At the gate entrance there is a small building with a combination lock, requiring four numbers, on the door.");
+	courtyard.addAdjacentRoom("lobby");
+	courtyard.addAdjacentRoom("guard house");
+	courtyard.addItem("");
+	
+	// ending room?
+	guardhouse.setName("guard house");
+	guardhouse.setDescription("There is a small television recording the surveillance of the different rooms onto tape. You begin to rewind the video, watching yourself explore the unknown surroundings. Once at the beginning of the tape, you see yourself laying on the operation table while a group of doctors inject you with syringes. You immediately remove yourself from the table and begin attacking the doctors. Terrified they flee, but cannot outrun you, as you attack them. Then, as if nothing happened, you slowly walk into the white-padded room, locking yourself inside.");
+	guardhouse.addAdjacentRoom("courtyard");
+	guardhouse.addItem("");
 
-  patientroom3.setName("patient room 3");
-  patientroom3.setDescription("The room is empty and there are hundreds of scratches and marks on the wooden-paneled walls. Among these marks you notice 4 distinct combinations of letters. ‘SN’, ‘AK’,  ‘EB’, and  IT’.");
-  patientroom3.addAdjacentRoom("hallway 2nd floor");
-  patientroom3.addItem("letter combination");
-
-  bathroom.setName("bathroom");
-  bathroom.setDescription("A mirror is located on the adjacent wall to the entrance. Looking into it you see yourself. With blood-shot eyes and veins protruding from your neck, you stare in disbelief. You see a shadow walk behind you in the mirror, but turn around to see nothing.");
-  bathroom.addAdjacentRoom("hallway 2nd floor");
-  bathroom.addItem("");
-
-  hallway2ndfloor.setName("hallway 2nd floor");
-  hallway2ndfloor.setDescription("");
-  hallway2ndfloor.addAdjacentRoom("patient room 1");
-  hallway2ndfloor.addAdjacentRoom("patient room 2");
-  hallway2ndfloor.addAdjacentRoom("patient room 3");
-  hallway2ndfloor.addAdjacentRoom("bathroom");
-  hallway2ndfloor.addAdjacentRoom("stairs");
-  hallway2ndfloor.addItem("");
-
-  stairs.setName("stairs");
-  stairs.setDescription("");
-  stairs.addAdjacentRoom("hallway 2nd floor");
-  stairs.addAdjacentRoom("lobby");
-  stairs.addItem("");
-
-  lobby.setName("lobby");
-  lobby.setDescription("What once was a nice entrance to the building is in disarray. Planted pots have been turned and the windows are shattered. Blood stains lead out of the building and a severed head is in the corner of the room.");
-  lobby.addAdjacentRoom("hallway 1st floor");
-  lobby.addAdjacentRoom("courtyard");
-  lobby.addAdjacentRoom("stairs");
-  lobby.addItem("severed head");
-
-  wardenoffice.setName("warden office");
-  wardenoffice.setDescription("The room is nicely decorated with rich colored wood and a large leather chair. From the ceiling, the entrails of an unknown creature hang. Below this there is a pile of blood-splattered documents. One detailing the layout of the building.");
-  wardenoffice.addAdjacentRoom("hallway 1st floor");
-  wardenoffice.addItem("map");
-
-  lab.setName("lab");
-  lab.setDescription("On the wall is a diagram of the human body with marks that seem to pin-point locations to inject some kind of medication. The opposing side of the room has a table with empty vials and a patient listing. The listing details information about the different patients and what room they belong to on the second floor.");
-  lab.addAdjacentRoom("hallway 1st floor");
-  lab.addItem("patient listing");
-
-  operatingroom.setName("operating room");
-  operatingroom.setDescription("An examination table is centered in the room with demolished leather restraints. On the ground are numerous syringes that are empty.");
-  operatingroom.addAdjacentRoom("hallway 1st floor");
-  operatingroom.addItem("empty syringes");
-
-  // starting room
-  solitaryconfinement.setName("solitary confinement");
-  solitaryconfinement.setDescription("You have awoken to find yourself laying in the middle of a white-padded room. The single light-bulb above you flickers inconsistently, and there are no signs of life around you. Across from you there is a battered door that appears to be locked.");
-  solitaryconfinement.addAdjacentRoom("hallway 1st floor");
-  solitaryconfinement.addItem("hair pin");
-  solitaryconfinement.addItem("tattered clothing");
-
-  hallway1stfloor.setName("hallway 1st floor");
-  hallway1stfloor.setDescription("");
-  hallway1stfloor.addAdjacentRoom("lab");
-  hallway1stfloor.addAdjacentRoom("operating room");
-  hallway1stfloor.addAdjacentRoom("solitary confinement");
-  hallway1stfloor.addAdjacentRoom("warden office");
-  hallway1stfloor.addAdjacentRoom("lobby");
-  hallway1stfloor.addItem("");
-
-  courtyard.setName("courtyard");
-  courtyard.setDescription("It is slightly raining and there are muddy footprints leading out of the now closed front gate. The entire yard is surrounded by a large fence topped with barbed wire. At the gate entrance there is a small building with a combination lock, requiring four numbers, on the door.");
-  courtyard.addAdjacentRoom("lobby");
-  courtyard.addAdjacentRoom("guard house");
-  courtyard.addItem("");
-
-  // ending room?
-  guardhouse.setName("guard house");
-  guardhouse.setDescription("There is a small television recording the surveillance of the different rooms onto tape. You begin to rewind the video, watching yourself explore the unknown surroundings. Once at the beginning of the tape, you see yourself laying on the operation table while a group of doctors inject you with syringes. You immediately remove yourself from the table and begin attacking the doctors. Terrified they flee, but cannot outrun you, as you attack them. Then, as if nothing happened, you slowly walk into the white-padded room, locking yourself inside.");
-  guardhouse.addAdjacentRoom("courtyard");
-  guardhouse.addItem("");
-
-  map = 
+	map = 
 	"  Second Floor_______________________						\r\n"
 	"  |                |                |						\r\n"
 	"  |    Bathroom    |    Patient     |						\r\n"
@@ -140,7 +140,7 @@ void game::gameInit()
 	"  |    Confinement |    Office      |              |        |  House    |	\r\n"
 	"  |________________|________________|______________|        |___________|	\r\n";
 
-  drawing = 
+	drawing = 
 	"      (                      )			\r\n"
 	"      ||    _,--------._    / |		\r\n"
 	"      | `.,'            `. /  |		\r\n"
