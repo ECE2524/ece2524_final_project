ff1e803242c0e6f1ffadda07acc0864d072186c7
Jay Pennington <jpenn92@vt.edu>
2013-12-13 16:29:18 -0500

    changed tabbed indenting

--- player.h
+++ player.h
@@ -7,17 +7,17 @@ using namespace std;
 class player
 {
 private:
-  // linked list stores player inventory items
-  list<string> inventory;
+	// linked list stores player inventory items
+	list<string> inventory;
 public:
-  // class constructor
-  player();
-  // class destructor
-  ~player();
-  // add an item to the player inventory
-  void addInventory(string);
-  // check if an item is in the player inventory
-  bool isInventory(string);
-  // print the items the player inventory
-  void printInventory();
+	// class constructor
+	player();
+	// class destructor
+	~player();
+	// add an item to the player inventory
+	void addInventory(string);
+	// check if an item is in the player inventory
+	bool isInventory(string);
+	// print the items the player inventory
+	void printInventory();
 };
